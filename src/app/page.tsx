c36c8e95fdcf641e02e1b98419e63b626c122860
Ken Greeff <ken@kengreeff.com>
2023-08-11 14:18:44 +1000

    Add Book quote section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
+import BookFreeQuote from "@/components/HomePage/BookFreeQuote";
 import Hero from "@/components/HomePage/Hero";
 import InstallationLocations from "@/components/HomePage/InstallationLocations";
 import Intro from "@/components/HomePage/Intro";
 import KeyFeatures from "@/components/HomePage/KeyFeatures";
 import MainNavigation from "@/components/MainNavigation";
 import Products from "@/components/HomePage/Products";
-import Questions from "@/components/HomePage/Questions";
 import Reference from "@/components/HomePage/Reference";
 import Statistics from "@/components/HomePage/Statistics";
 
@@ -21,7 +21,7 @@ function Home() {
       <Reference />
       <Products />
 
-      <Questions />
+      <BookFreeQuote />
     </main>
   )
 }
