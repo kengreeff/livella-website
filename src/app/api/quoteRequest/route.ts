ee8cfa733f9b3d5aed502f410fa3d195697c8f9c
Ken Greeff <ken@kengreeff.com>
2023-10-26 13:21:34 +1100

    Send email for quote request

--- src/app/api/quoteRequest/route.ts
+++ src/app/api/quoteRequest/route.ts
@@ -1,3 +1,7 @@
+import sendgrid from '@sendgrid/mail'
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY || '')
+
 import contentfulClient from "@/utils/contentfulClient"
 
 export async function POST(request: Request) {
@@ -17,6 +21,8 @@ export async function POST(request: Request) {
       requirements,
       rooms = [],
     } = jsonBody
+
+    const fullName = `${firstName} ${lastName}`
 
     const space = await contentfulClient.getSpace(process.env.CONTENTFUL_SPACE_ID || '')
     const environment = await space.getEnvironment(process.env.CONTENTFUL_ENVIRONMENT || 'master')
@@ -70,8 +76,30 @@ export async function POST(request: Request) {
       }
     })
 
-    // // Send Email to the Team
-    
+    // Send Email
+    try {
+      await sendgrid.send({
+        to: process.env.LIVELLA_CONTACT_EMAIL || 'info@livella.com',
+        from: {
+          name: 'Livella Enquiries',
+          email: 'noreply@websiteenquiries.com',
+        },
+        replyTo: {
+          name: `${fullName}`,
+          email,
+        },
+        subject: `Quote Request from ${fullName}`,
+        text: `
+          Project Address: ${projectAddress}\n
+          Phone: ${phone}\n
+          Project Type: ${projectType}\n
+          Requirements: ${requirements}\n
+          Link: https://app.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/entries/${entry?.sys?.id}
+        `,
+      })
+    } catch (error: any) {
+      console.log({ error })
+    }
 
     return Response.json({ success: true, entry  })
   } catch (error) {
